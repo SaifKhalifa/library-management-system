60f66e06a2a601dd39dc7f2bab4def776003551a
Saif Khalifa <63987779+SaifKhalifa@users.noreply.github.com>
2025-05-02 20:51:39 +0300

    DDL Queries for creating tables and database

---
+++ sql/DDL_create_tables.sql
@@ -0,0 +1,41 @@
+-- ===========================
+-- Library Management System
+-- Database Schema Definition
+-- ===========================
+
+-- Database Creation
+CREATE DATABASE LibraryManagmentSystem;
+
+-- Table: Books
+CREATE TABLE Books (
+    BookID INT PRIMARY KEY,
+    Title VARCHAR(50) NOT NULL,
+    Author VARCHAR(20),
+    ISBN VARCHAR(50),
+    PublishedDate DATE,
+    Genre VARCHAR(20),
+    ShelfLocation VARCHAR(50),
+    CurrentStatus VARCHAR(20) CHECK (CurrentStatus IN ('Available', 'Borrowed')) -- restricts the values to be only two options.
+);
+
+-- Table: Borrowers
+CREATE TABLE Borrowers (
+    BorrowerID INT PRIMARY KEY,
+    FirstName VARCHAR(20) NOT NULL,
+    LastName VARCHAR(20) NOT NULL,
+    Email VARCHAR(20) UNIQUE,
+    DateOfBirth DATE,
+    MembershipDate DATE
+);
+
+-- Table: Loans
+CREATE TABLE Loans (
+    LoanID INT PRIMARY KEY,
+    BookID INT,
+    BorrowerID INT,
+    DateBorrowed DATETIME NOT NULL,
+    DueDate DATETIME NOT NULL,
+    DateReturned DATETIME,
+    FOREIGN KEY (BookID) REFERENCES Books(BookID),
+    FOREIGN KEY (BorrowerID) REFERENCES Borrowers(BorrowerID)
+);
